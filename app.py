e82a07b3b9e7e6b7572b0d0b111ca2f4a49168dc
Joshua Grant <joshua.m.grant@gmail.com>
2017-03-07 15:01:28 -0500

    basic idea is working for generating random values

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
 from numpy import random
 
 app = Flask(__name__)
@@ -18,24 +18,27 @@ samples = [
     }
 ]
 
+@app.errorhandler(404)
+def invalid_value(error):
+    return make_response(jsonify({'error': 'Invalid sample value, must be positive integer'}), 404)
+
 @app.route('/corsica/help', methods=['GET'])
 def get_help():
     h = {
         'title' : u'Corsica - Restful Random Samples',
-        'description' : u'Return a list of samples of a desired distribution. Supported options are uniform[a,b], binomial(n,p) and exponential(l)'
+        'description' : u'Return a list of samples of a desired distribution. Supported options are uniform[0,1], binomial(n,p) and exponential(l)'
     }
     return jsonify(h)
 
-@app.route('/corsica/samples/<int:sample_id>', methods=['GET'])
-def get_samples(sample_id):
-    sample = [s for s in samples if s['id'] == sample_id]
-    if len(sample) == 0:
+@app.route('/corsica/samples/uniform/<int:samples>', methods=['GET'])
+def get_uniform(samples):
+    if samples <= 0:
         abort(404)
-    return jsonify({'sample' : sample[0]})
+    
+    r = random.uniform(0,1,samples).tolist()
+    return jsonify({'samples' : r })
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Invalid distribution name'}), 404)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
