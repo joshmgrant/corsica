02dc807570aa7d23ad593d263d62bc45283dde41
Joshua Grant <joshua.m.grant@gmail.com>
2017-03-07 16:07:57 -0500

    better defaults

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from numpy import random
 
 app = Flask(__name__)
 
+
 @app.errorhandler(400)
 def invalid_value(error):
     return make_response(jsonify({'error': 'Invalid data request'}), 400)
@@ -16,7 +17,7 @@ def get_help():
     return jsonify(h)
 
 @app.route('/corsica/samples/uniform/', methods=['GET'])
-def get_uniform(a=0.0, b=1.0, n=1):
+def get_uniform(a=0.0, b=1.0, n=10):
     a = request.args.get('a', a)
     b = request.args.get('b', b)
     samples = request.args.get('n', n)
@@ -37,7 +38,7 @@ def get_uniform(a=0.0, b=1.0, n=1):
     return jsonify({'samples' : r })
 
 @app.route('/corsica/samples/normal/', methods=['GET'])
-def get_normal_dist(mu=0, sigma=1, n=1):
+def get_normal_dist(mu=0.0, sigma=1.0, n=10):
     m = request.args.get('mu', mu)
     s = request.args.get('sigma', sigma)
     samples = request.args.get('n', n)
@@ -53,7 +54,7 @@ def get_normal_dist(mu=0, sigma=1, n=1):
     return jsonify({'samples' : r })
 
 @app.route('/corsica/samples/exponential/', methods=['GET'])
-def get_exponential(l=1.0, n=1):
+def get_exponential(l=1.0, n=10):
     m = request.args.get('l', l)
     samples = request.args.get('n', n)
 
