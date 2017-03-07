d6351cb2a7aaa1c4ff1636217a8a633759c1c5cc
Joshua Grant <joshua.m.grant@gmail.com>
2017-03-07 15:26:08 -0500

    commands are being completed but not correctly

--- app.py
+++ app.py
@@ -3,21 +3,6 @@ from numpy import random
 
 app = Flask(__name__)
 
-samples = [
-    {
-        'id': 1,
-        'title': u'Uniform random variables',
-        'description': u'Generate a list of uniform random samples.', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Binomial random variables',
-        'description': u'Generate a list of binomial random samples', 
-        'done': False
-    }
-]
-
 @app.errorhandler(404)
 def invalid_value(error):
     return make_response(jsonify({'error': 'Invalid data request'}), 404)
@@ -26,15 +11,19 @@ def invalid_value(error):
 def get_help():
     h = {
         'title' : u'Corsica - Restful Random Samples',
-        'description' : u'Return a list of samples of a desired distribution. Supported options are uniform[0,1], binomial(n,p) and exponential(l)'
+        'description' : u'Return a list of samples of a desired distribution. Supported options are uniform[a,b], normal(mean, sigma) and exponential(l)'
     }
     return jsonify(h)
 
 @app.route('/corsica/samples/uniform/', methods=['GET'])
-def get_uniform(a=0, b=1, n=1):
+def get_uniform(a=0.0, b=1.0, n=1):
     a = request.args.get('a', a)
     b = request.args.get('b', b)
     samples = request.args.get('n', n)
+
+    a = float(a)
+    b = float(b)
+    samples = int(samples)
 
     if a > b:
         a, b = b, a
@@ -45,6 +34,22 @@ def get_uniform(a=0, b=1, n=1):
         abort(404)
     
     r = random.uniform(a, b, samples).tolist()
+    return jsonify({'samples' : r })
+
+@app.route('/corsica/samples/normal/', methods=['GET'])
+def get_normal_dist(mean=0, sigma=1, n=1):
+    m = request.args.get('mean', mean)
+    s = request.args.get('sigma', sigma)
+    samples = request.args.get('n', n)
+
+    m = float(m)
+    s = float(s)
+    samples = int(samples)
+
+    if samples <= 0:
+        abort(404)
+    
+    r = random.normal(m, s, samples).tolist()
     return jsonify({'samples' : r })
 
 if __name__ == '__main__':
