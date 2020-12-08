9870b57784842eaa902150a53670e452e4d1763c
joshgrant <joshua.m.grant@gmail.com>
2020-12-07 22:50:20 -0500

    swapped out random.random for numpy random

--- corsica/distributions.py
+++ corsica/distributions.py
@@ -1,4 +1,4 @@
-import random
+import numpy.random as rand
 
 
 def uniform(size: int = 10, lower_bound: float = 0.0, upper_bound: float = 1.0) -> list:
@@ -14,7 +14,7 @@ def uniform(size: int = 10, lower_bound: float = 0.0, upper_bound: float = 1.0) 
     if lower_bound > upper_bound:
         lower_bound, upper_bound = upper_bound, lower_bound
 
-    return [random.uniform(lower_bound, upper_bound) for i in range(size)]
+    return [(upper_bound - lower_bound)*rand.random() + lower_bound for i in range(size)]
 
 
 def normal(size: int = 10, mu: float = 0.0, sigma: float = 1.0) -> list:
@@ -24,11 +24,11 @@ def normal(size: int = 10, mu: float = 0.0, sigma: float = 1.0) -> list:
     if sigma is None:
         raise TypeError("Expected upper bound, did not receive one")
 
-    return [random.gauss(mu, sigma) for i in range(size)]
+    return [rand.normal(mu, sigma) for i in range(size)]
 
 
 def exponential(size: int = 10, lam: float = 1.0) -> list:
     if lam <= 0.0:
         raise TypeError('Lambda parameter must be positive')
 
-    return [random.expovariate(lam) for i in range(size)]
+    return [rand.exponential(lam) for i in range(size)]
