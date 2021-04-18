1ac08672ac85674d851dc527b3ad8c632d88eb15
joshgrant <joshua.m.grant@gmail.com>
2021-04-18 15:02:26 -0400

    added allow-cross-origin for public endpoints

--- main.py
+++ main.py
@@ -2,11 +2,25 @@ from typing import Optional
 
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from corsica.distributions import uniform
 from corsica.distributions import normal
 from corsica.distributions import exponential
 
+origins = [
+    "https://corsica.joshgrant.online/",
+    "http://corsica.joshgrant.online/",
+    "http://localhost:5000/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI(
     title="Corisca - Random Numbers as a Service",
