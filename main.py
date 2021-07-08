c1763628669cc03ec6b07e73c48ebd6e4ccfd970
joshin4colours <joshua.m.grant@gmail.com>
2021-07-07 22:56:31 -0400

    Honeycomb opentelemetry (#13)
    * added tracing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Optional
+import os
 
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
@@ -7,6 +8,41 @@ from fastapi.middleware.cors import CORSMiddleware
 from corsica.distributions import uniform
 from corsica.distributions import normal
 from corsica.distributions import exponential
+
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from grpc import ssl_channel_credentials
+
+
+# resource describes app-level information that will be added to all spans
+resource = Resource(attributes={
+    "service.name": "corsica"
+})
+
+# create new trace provider with our resource
+trace_provider = TracerProvider(resource=resource)
+
+# create exporter to send spans to Honeycomb
+otlp_exporter = OTLPSpanExporter(
+    endpoint="api.honeycomb.io:443",
+    insecure=False,
+    credentials=ssl_channel_credentials(),
+    headers=(
+        ("x-honeycomb-team", os.environ.get("HONEYCOMB_API_KEY")),
+        ("x-honeycomb-dataset", "corsica_overall")
+    )
+)
+
+# register exporter with provider
+trace_provider.add_span_processor(
+    BatchSpanProcessor(otlp_exporter)
+)
+
+# register trace provider
+trace.set_tracer_provider(trace_provider)
 
 
 app = FastAPI(
@@ -38,7 +74,11 @@ def read_root():
          summary="Continuous Uniform Distribution - Single Value",
          description="Returns a single random value from a standard uniform distribution")
 def get_single_uniform():
-    value = uniform(1, 0.0, 1.0)
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("http-handler"):
+        with tracer.start_as_current_span("uniform_single"):
+            value = uniform(1, 0.0, 1.0)
+
     return {"value": value}
 
 
@@ -53,7 +93,11 @@ def get_uniform(size: int, lower_bound: Optional[float] = None, upper_bound: Opt
     if lower_bound > upper_bound:
         lower_bound, upper_bound = upper_bound, lower_bound
 
-    values_list = uniform(size, lower_bound, upper_bound)
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("http-handler"):
+        with tracer.start_as_current_span("uniform_list"):
+            values_list = uniform(size, lower_bound, upper_bound)
+
     return {"lower": lower_bound, "upper": upper_bound, "values": values_list}
 
 
@@ -62,7 +106,11 @@ def get_uniform(size: int, lower_bound: Optional[float] = None, upper_bound: Opt
          summary="Continuous Normal Distribution - Single Value",
          description="Returns a single random value from a standard normal distribution")
 def get_single_normal():
-    value = normal(1, 0.0, 1.0)
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("http-handler"):
+        with tracer.start_as_current_span("normal_single"):
+            value = normal(1, 0.0, 1.0)
+
     return {"value": value}
 
 
@@ -74,7 +122,11 @@ def get_normal(size: int, mu: Optional[float] = None, sigma: Optional[float] = N
     mu = mu if mu else 0.0
     sigma = sigma if sigma else 1.0
 
-    values_list = normal(size, mu, sigma)
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("http-handler"):
+        with tracer.start_as_current_span("normal_list"):
+            values_list = normal(size, mu, sigma)
+
     return {"mu": mu, "sigma": sigma, "values": values_list}
 
 
@@ -83,7 +135,11 @@ def get_normal(size: int, mu: Optional[float] = None, sigma: Optional[float] = N
          summary="Continuous Exponential Distribution - Single Value",
          description="Returns a single random value from a standard exponential distribution")
 def get_single_exponential():
-    value = exponential(1, 1.0)
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("http-handler"):
+        with tracer.start_as_current_span("exponential_single"):
+            value = exponential(1, 1.0)
+
     return {"value": value}
 
 
@@ -94,5 +150,9 @@ def get_single_exponential():
 def get_exponential(size: int, lam: Optional[float] = None):
     lam = lam if lam else 1.0
 
-    values_list = exponential(size, lam)
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("http-handler"):
+        with tracer.start_as_current_span("exponential_list"):
+            values_list = exponential(size, lam)
+
     return {"lambda": lam, "values": values_list}
